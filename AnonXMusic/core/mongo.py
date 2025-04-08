bba6dc5e45e9c6f99efd028ec928701354bb6726
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-04-08 10:18:52 +0530

    Update mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -4,11 +4,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("✦ Connecting to your Mongo Database...💛")
+LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER(__name__).info("✦ Connected to your Mongo Database...❤️")
+    mongodb = _mongo_async_.TGN
+    LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
-    LOGGER(__name__).error("✦ Failed to connect to your Mongo Database...💚")
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
